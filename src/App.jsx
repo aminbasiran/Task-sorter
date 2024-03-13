b1720681ca1b6bcf53014544a896186a57213e24
aminbasiran <work.aminbasiran@gmail.com>
2024-03-13 23:40:58 +0800

    add toast, but needs to fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import AddTask from './components/AddTask'
 import TaskLists from './components/TaskLists'
 import TextBox from './components/TextBox'
 
+import { FaCheck } from "react-icons/fa";
+
 
 function App() {
 
@@ -19,6 +21,7 @@ function App() {
   const [prioritize,setPrioritize] = useState(false)
   const [frequentElement,setFrequentElement] = useState({})
   const [text,setText] = useState("")
+  const [toast,setToast] = useState(false)
 
 
   function saveTasksToLocalStorage(tasks){
@@ -55,15 +58,14 @@ function App() {
   }
 
   return (
-
-      <div className='flex flex-col  gap-2 pla '>
+      <div className='flex flex-col gap-2 w-full overflow-x-hidden  '>
         <div className='flex flex-col items-start'>
           <h1 className='text-cyan-500 text-5xl font-extrabold'>Sortify!</h1>
           <h1 className='text-md font-semibold text-zinc-500'>Your friendly task sorter.</h1>
         </div>
         {/* <h1 className='text-xs text-centerfont-semibold text-zinc-500'>This app utilizes the prioritization matrices to determine the order of priority.</h1> */}
         <div className='flex flex-col gap-2 p-3 bg-white/50 shadow-lg rounded-md flex-grow'>
-            <AddTask saveTasksToLocalStorage={saveTasksToLocalStorage} tasks={tasks} setTasks={setTasks} text={text} setText={setText}/>
+            <AddTask saveTasksToLocalStorage={saveTasksToLocalStorage} tasks={tasks} setTasks={setTasks} text={text} setText={setText} setToast={setToast}/>
             <TaskLists tasks={tasks} setTasks={setTasks}/>
             <button onClick={handleOpenTextBox} className='text-xs px-2 py-1 rounded-md text-white font-semibold bg-cyan-600' type='button'>Prioritize</button>
             {prioritize && <TextBox tasks={tasks} setPrioritize={setPrioritize} setFrequentElement={setFrequentElement} frequencyStore={frequencyStore} setFrequencyStore={setFrequencyStore}/> }
@@ -81,6 +83,15 @@ function App() {
         </div>
         <div className='float-cyan absolute w-[200px] aspect-square bg-cyan-300 bottom-[200px] left-[60px] blur-[110px] -z-20'></div>
         <div className='absolute w-[100px] aspect-square bg-fuchsia-600 bottom-[160px] right-0 blur-[80px] -z-20'></div>
+        <div className={` ${toast ?  "translate-x-36" : "translate-x-96" } top-3 absolute transition-all duration-700 ease-in-out  bg-white shadow-lg flex items-center gap-4 pl-4 pr-8  py-2 rounded-md`}>
+                <div className='bg-green-600 p-1 rounded-full'>
+                    <FaCheck color="white" size={10} />
+                </div>
+                <div className='flex flex-col items-start'>
+                    <h1 className='text-xs font-bold'>Success</h1>
+                    <h1 className='text-xs'>{text ? text : "temp"} has been added!</h1>
+                </div>
+            </div> 
       </div>
   )
 }
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,9 @@
 import React,{useState} from 'react'
 
-const AddTask = ({tasks, setTasks,text,setText,saveTasksToLocalStorage}) => {
 
+const AddTask = ({tasks, setTasks,text,setText,saveTasksToLocalStorage,setToast}) => {
+
+    
 
     const addToList = () => {
         if(!text){
@@ -14,14 +16,19 @@ const AddTask = ({tasks, setTasks,text,setText,saveTasksToLocalStorage}) => {
             setText("")
             return 
         }
-
         setTasks(prevTasks => {
             const updatedTasks = [...prevTasks, text];
             saveTasksToLocalStorage(updatedTasks); // Save tasks to localStorage
             return updatedTasks; // Return updated tasks array
         });
         
+        
+        setToast(prev => !prev)
         setText("")
+
+        setTimeout(() => {
+            setToast(prev => !prev)
+        }, 2000);
     }
 
     const handleKeyDown = (event) => {
@@ -34,23 +41,31 @@ const AddTask = ({tasks, setTasks,text,setText,saveTasksToLocalStorage}) => {
             if (tasks.includes(text)) {
                 alert("Task already exists");
                 setText("");
+                
                 return;
             }
-    
             setTasks(prevTasks => {
                 const updatedTasks = [...prevTasks, text];
                 saveTasksToLocalStorage(updatedTasks); // Save tasks to localStorage
                 return updatedTasks; // Return updated tasks array
             });
-    
+            
+            
+            setToast(prev => !prev)
             setText("");
+
+            setTimeout(() => {
+                setToast(prev => !prev)
+            }, 2000);
         }
     };
 
     return (
-        <div className='flex flex-row gap-3 justify-between border-b-2 p-2 '>
-            <input className='text-xs appearance-none bg-transparent flex-grow p-1' type="text" value={text} onChange={e => setText(e.target.value)} onKeyDown={handleKeyDown} placeholder='Enter task'/>
-            <button className=' text-xs px-2 py-1 rounded-md text-white font-semibold bg-cyan-600' onClick={addToList} type='button'>Add task</button>
+        <div className=''>
+            <div className='flex flex-row gap-3 justify-between border-b-2 p-2 '>
+                <input className='text-xs appearance-none bg-transparent flex-grow p-1' type="text" value={text} onChange={e => setText(e.target.value)} onKeyDown={handleKeyDown} placeholder='Enter task'/>
+                <button className=' text-xs px-2 py-1 rounded-md text-white font-semibold bg-cyan-600' onClick={addToList} type='button'>Add task</button>
+            </div>
         </div>
     )
 }
